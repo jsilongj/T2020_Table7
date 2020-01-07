d9d0a63fa1380650e4f2cd25fcf38219bb2f77e0
fangliyan96 <fangliyan96@gmail.com>
2020-01-07 15:00:47 +0800

    pull inbox msg

--- js/inbox_messages.js
+++ js/inbox_messages.js
@@ -6,28 +6,17 @@ const header = {
   'token': token,
 }
 
+// const johnId = "1"
+// const johnAccount = "10"
 
 async function getMessages(customerId) {
   try {
-    const response = await fetch(`http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/accounts/deposit/${customerId}`, {
+    const response = await fetch(`http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/message/${customerId}`, {
       method: 'GET',
       headers: header,
     });
-
     return await response.json();
   } catch (err) {
     console.log(err);
   }
-}
-
-function inbox() {
-  let username = document.getElementById("username").value;
-  getMessages(username).then((res) =>
-  {
-    if (res.userName == null){
-    alert("Error Password or Username")
-  }else{window.location = "Home.html";;}
-    console.log(res)
-  });
-
 }
